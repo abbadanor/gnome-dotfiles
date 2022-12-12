f0ce2c0cad86f0f3cd5624951cd0d4c0cbc5d7eb
Adam Nord <adam.nord@abbgymnasiet.se>
2022-12-12 19:06:22 +0100

    first commit

---
+++ doom/.doom.d/config.el
@@ -0,0 +1,117 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Adam Nord"
+      user-mail-address "adam.nord04@gmail.com")
+
+;; Move to trash instead of rm:ing
+(setq delete-by-moving-to-trash t)
+
+(setq default-frame-alist '((undecorated . t)))
+
+(scroll-bar-mode -1)
+
+(beacon-mode 1)
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "JetBrains Mono" :size 15)
+      doom-variable-pitch-font (font-spec :family "Roboto" :size 15)
+      doom-big-font (font-spec :family "JetBrains Mono" :size 24))
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+(setq org-hide-emphasis-markers t)
+
+(let* ((variable-tuple
+        (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
+        ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+        ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+        ((x-list-fonts "Verdana")         '(:font "Verdana"))
+        ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+        (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+        (headline           `(:inherit default :weight bold)))
+
+(custom-theme-set-faces
+'user
+`(org-level-8 ((t (,@headline ,@variable-tuple))))
+`(org-level-7 ((t (,@headline ,@variable-tuple))))
+`(org-level-6 ((t (,@headline ,@variable-tuple))))
+`(org-level-5 ((t (,@headline ,@variable-tuple))))
+`(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+`(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+`(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+`(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+`(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
