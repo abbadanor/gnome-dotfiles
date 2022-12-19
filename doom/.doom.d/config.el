6171e16c31656430349488ee9b487b8ea4cb3445
Adam Nord <adam.nord@abbgymnasiet.se>
2022-12-19 20:06:40 +0100

    removed broken org headlines

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -60,30 +60,6 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-(setq org-hide-emphasis-markers t)
-
-(let* ((variable-tuple
-        (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
-        ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-        ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-        ((x-list-fonts "Verdana")         '(:font "Verdana"))
-        ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-        (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-        (headline           `(:inherit default :weight bold)))
-
-(custom-theme-set-faces
-'user
-`(org-level-8 ((t (,@headline ,@variable-tuple))))
-`(org-level-7 ((t (,@headline ,@variable-tuple))))
-`(org-level-6 ((t (,@headline ,@variable-tuple))))
-`(org-level-5 ((t (,@headline ,@variable-tuple))))
-`(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-`(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-`(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-`(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-`(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
-
-
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
