48fda4e66f07f1a73911c1720c0fcd13d24cb58a
Adam Nord <adam.nord04@gmail.com>
2024-03-17 00:52:59 +0100

    made a real merge

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -203,6 +203,7 @@ require('lazy').setup({
   require 'plugins.rust',
   require 'plugins.neorg',
   require 'plugins.zen-mode',
+  -- require 'plugins.autochdir',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
